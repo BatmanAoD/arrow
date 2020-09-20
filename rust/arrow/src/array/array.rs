d741c5edf5a8ea277f0175dcaed24ed3726f8c6e
Kyle Strand <kyle.strand@pieinsurance.com>
2020-09-20 13:55:16 -0600

    Remove print_long_array

--- rust/arrow/src/array/array.rs
+++ rust/arrow/src/array/array.rs
@@ -1192,38 +1192,6 @@ where
     return Ok(())
 }
 
-// Helper function for printing potentially long arrays.
-fn print_long_array<A, F>(array: &A, f: &mut fmt::Formatter, print_item: F) -> fmt::Result
-where
-    A: Array,
-    F: Fn(&A, usize, &mut fmt::Formatter) -> fmt::Result,
-{
-    for i in 0..std::cmp::min(10, array.len()) {
-        if array.is_null(i) {
-            writeln!(f, "  null,")?;
-        } else {
-            write!(f, "  ")?;
-            print_item(&array, i, f)?;
-            writeln!(f, ",")?;
-        }
-    }
-    if array.len() > 10 {
-        if array.len() > 20 {
-            writeln!(f, "  ...{} elements...,", array.len() - 20)?;
-        }
-        for i in array.len() - 10..array.len() {
-            if array.is_null(i) {
-                writeln!(f, "  null,")?;
-            } else {
-                write!(f, "  ")?;
-                print_item(&array, i, f)?;
-                writeln!(f, ",")?;
-            }
-        }
-    }
-    Ok(())
-}
-
 impl fmt::Debug for ListArray {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "ListArray\n[\n")?;
